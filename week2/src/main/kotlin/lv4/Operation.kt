66320e5e92b635a10b1a10c552b3a8ed7f2f6470
eunhye han <hellou8363@gmail.com>
2024-04-30 20:11:14 +0900

    🐛 나눗셈 수행 시 0으로 나눌 경우에 대해 처리가 없던 부분 예외 추가

--- week2/src/main/kotlin/lv4/Operation.kt
+++ week2/src/main/kotlin/lv4/Operation.kt
@@ -8,4 +8,16 @@ val substractOperation = Operation { numbers: List<Number> -> numbers[0].toLong(
 
 val multiplyOperation = Operation { numbers: List<Number> -> numbers[0].toLong() * numbers[1].toLong() }
 
-val dvideOperation = Operation { numbers: List<Number> -> numbers[0].toDouble() / numbers[1].toDouble() }
+class DivideOperation : Operation {
+    override fun operate(numbers: List<Number>): Number {
+
+        println("numbers = ${numbers}")
+        println(numbers[0] == 0)
+        println(numbers[1])
+        if (numbers[0] == 0L || numbers[1] == 0L) {
+            throw ArithmeticException("0으로 나눌 수 없습니다.")
+        }
+
+        return numbers[0].toDouble() / numbers[1].toDouble()
+    }
+}
