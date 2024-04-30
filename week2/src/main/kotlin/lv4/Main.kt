6bdb5facd98099e74adc369c54ec450421c436db
eunhye han <hellou8363@gmail.com>
2024-04-30 20:14:23 +0900

    🐛 ♻️ 단순 출력문들을 모아놓은 클래스를 삭제하면서 출력문들을 main으로 이동, 예외처리 추가

--- week2/src/main/kotlin/lv4/Main.kt
+++ week2/src/main/kotlin/lv4/Main.kt
@@ -1,22 +1,50 @@
 package lv4
 
+import Calculator
+
 fun main() {
-    val guidance = Guidance()
+    val calculator = Calculator()
+    var order = 0
+    lateinit var result: Number
 
-    while (true) {
-        guidance.usageGuide()
+    println("========= 계산기 ==========")
+    println("계산할 식을 입력해주세요.")
+    println("예) 2 + 5 * 4 / 3")
+    println("계산기 종료 : x")
+    print("계산식 : ")
 
-        val input = readln()
+    // 사용자에게 계산식을 입력 받음
+    val input = readln()
 
-        if (input == "x") {
-            println("계산기를 종료합니다.")
-            break
-        } else {
-            try {
-                Storage().save(input)
-            } catch (e: NumberFormatException) {
-                println(e.message)
+    if (input == "x") { // 입력값이 "x"이면 종료
+        println("계산기를 종료합니다.")
+    } else {
+        try {
+            // 유효한 식인지 검증
+            val value = Validation().validateInputValue(input)
+            val symbols = value[0].map { it.toString() }
+            val numbers = value[1].map { it.toString().toLong() }
+
+            println(value[0])
+            println(value[1])
+
+            // 저장된 값으로 계산
+            result = numbers[order]
+            while (order < symbols.size) {
+                result = calculator.operate(symbols[order], arrayListOf(result, numbers[order + 1]))
+                order++
+                println("order ===> $order")
             }
+
+            println("계산된 결괏값을 안내드립니다.")
+            println("결괏값 : $result")
+
+
+            // 결과 출력
+        } catch (e: NumberFormatException) {
+            println(e.message)
+        } catch (e: Exception) {
+            println(e.message)
         }
     }
 }
